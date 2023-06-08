476d8f836944b575a80fa6b7c4ec8f8840eef532
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-06-08 16:13:51 +0200

    recherche d'erreur de création du array

--- Portfolio-architecte-sophie-bluel/FrontEnd/assets/javascript/works.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/javascript/works.js
@@ -1,31 +1,58 @@
 const gallery = document.querySelector(".gallery");
 gallery.innerHTML = "";
 let dataListForHtml = [];
+let dataCardListForHtml = [];
+let dataFilterListForHtml = [];
 
-function GetDataForHtml() {
+function getDataForHtml() {
     fetch("http://localhost:5678/api/works")
-    .then(data => data.json())
-    .then(DataJson => {
-        dataListForHtml = DataJson;
-    })
-}
-
-GetDataForHtml();
-console.log(dataListForHtml);
-
-function createHtml() {
-    dataListForHtml.forEach(element => {
-        let figure = document.createElement("figure");
-        let img = document.createElement("img");
-        let title = document.createElement("figcaption");
-        img.src = element.img;
-        img.alt = element.title;
-        figcaption.innerHTML = element.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
+    .then(response => response.json())
+    .then(data => {
+        dataListForHtml = data;
     });
-}
+};
+function getFilterForHtml() {
+    fetch("http://localhost:5678/api/categories")
+    .then(response => response.json())
+    .then(filterData => {
+        dataFilterListForHtml = filterData;
+    });
+};
+getFilterForHtml();
+getDataForHtml();
 
-createHtml();
-console.log(createHtml);
+function createCardHtml(category = 0) {
+    gallery.innerHTML = "";
+    dataListForHtml.forEach(element => {
+        if ( category == 0 || element.category == category) {
+            let figure = document.createElement("figure");
+            let img = document.createElement("img");
+            let figcaption = document.createElement("figcaption");
+            img.src = element.img;
+            img.alt = element.title;
+            figcaption.innerHTML = element.title;
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        };
+    });
+};
+
+createCardHtml();
+
+function createFilterForHtml() {
+    filters.innerHTML = "";
+    dataFilterListForHtml.forEach(element => {
+        let li = document.createElement("li");
+        li.classList.add("filter");
+        li.dataset.categoryId = element.id;
+        li.innerHTML = element.name;
+        filters.appendChild(li);
+    });
+};
+
+document.querySelectorAll(".filter").forEach(element => {
+    element.addEventListener('click', function () {
+        createHtml(this.dataset.categoryId);
+    });
+});
