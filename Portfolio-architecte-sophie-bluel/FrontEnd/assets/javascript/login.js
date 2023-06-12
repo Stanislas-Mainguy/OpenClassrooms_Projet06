01f8edc62ec6a052e76c45fa51d6a42a408edc8e
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-06-12 12:31:17 +0200

    Test pour renvoi à la page d'acceuil

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/assets/javascript/login.js
@@ -0,0 +1,56 @@
+// Section des variables
+const loginButton = document.querySelector("#login_button");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const errorMessage = document.querySelector("#error-message");
+const emailError = document.querySelector("#email-error");
+const passwordError = document.querySelector("#password-error");
+
+// Vérifier si l'utilisateur est déjà connecté ou non //
+checkAuthentication();
+
+// Création de l'eventListener avec ses sous-sections //
+loginButton.addEventListener("click", function () {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  emailError.textContent = "";
+  passwordError.textContent = "";
+  emailInput.classList.remove("error");
+  passwordInput.classList.remove("error");
+
+  // Envoie de la requête de connection à L'API //
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email: email, password: password }),
+    credentials: "same-origin",
+  })
+    // Récupération du token depuis l'API //
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Les informations d'identification sont incorrectes.");
+      }
+    })
+    // Stockage du token et redirection vers la page d'accueil //
+    .then((data) => {
+      localStorage.setItem("token", data.token);
+      window.location.href = "./index.html";
+    })
+    .catch((error) => {
+      errorMessage.textContent = error.message;
+    });
+});
+
+// Fonction pour vérifier si l'utilisateur est connecté //
+function checkAuthentication() {
+  const token = localStorage.getItem("token");
+  if (token) {
+    console.log("Utilisateur connecté");
+  } else {
+    console.log("Utilisateur non connecté");
+  }
+}
