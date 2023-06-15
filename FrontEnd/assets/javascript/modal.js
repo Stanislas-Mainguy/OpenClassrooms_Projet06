f5e21bebbf4302019e46fcce7074671c161b3bb7
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-06-15 15:01:04 +0200

    Création d'un event clique et fix problem  padding

--- FrontEnd/assets/javascript/modal.js
+++ FrontEnd/assets/javascript/modal.js
@@ -1,2 +1,10 @@
 // Section des variables //
+const modalOpenings = document.querySelectorAll(".modal-opening");
 
+//Création d'un eventListener pour ouvreture de la modal sur chaque bouton de modif //
+modalOpenings.forEach((element) => {
+    element.addEventListener("click", () => {
+        const modal = document.getElementById("modal");
+        modal.style.display = "flex";
+    });
+  });
