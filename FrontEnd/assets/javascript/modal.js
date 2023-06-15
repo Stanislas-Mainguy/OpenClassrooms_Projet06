270d34744e1ca896b84b8ef02a3b66b20da3c25e
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-06-15 16:30:49 +0200

    Fix ouverture fermeture du modal

--- FrontEnd/assets/javascript/modal.js
+++ FrontEnd/assets/javascript/modal.js
@@ -1,17 +1,29 @@
 // Section des variables //
 const modalOpeningElements = document.querySelectorAll(".modal-opening");
-const modal = document.getElementById("modal");
-
-// Section des fonctions //
-
-    // Fonction d'ouverture du modal //
-    function openModal() {
-    modal.style.display = "flex";
-    };
+const modal = document.querySelector("#modal");
+const closeModal = document.querySelectorAll(".close-modal");
 
 // Section des eventListener //
 
     // Ouverture du modal //
     modalOpeningElements.forEach((element) => {
-    element.addEventListener("click", openModal);
+        element.addEventListener("click", () => {
+            modal.style.display = "flex";
+            document.body.classList.add("close-modal");
+        });
+    });
+
+    // Fermeture du modal
+    document.addEventListener("click", (event) => {
+        let clickedOutsideModal = true;
+        
+        modalOpeningElements.forEach((element) => {
+            if (element.contains(event.target)) {
+            clickedOutsideModal = false;
+            }
+        });
+        if (clickedOutsideModal && !modal.contains(event.target)) {
+            modal.style.display = "none";
+            document.body.classList.remove("close-modal");
+        }
     });
