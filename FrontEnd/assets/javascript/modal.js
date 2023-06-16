aba70cb6097e146813600d7ec8184f397bd92615
Stanislas Mainguy <stan.mainguy@gmail.com>
2023-06-16 15:31:44 +0200

    modification pour l'ouverture du modal

--- FrontEnd/assets/javascript/modal.js
+++ FrontEnd/assets/javascript/modal.js
@@ -4,44 +4,20 @@ const modal = document.querySelector("#modal");
 const closeModal = document.querySelectorAll(".close-modal");
 const arrayElement = document.querySelector("#array-element");
 const overlay = document.querySelector("#overlay");
-const closeModalIcons = document.querySelectorAll(".close-modal-icon");
 let arrayPictures = [];
 
 // Section des eventListener //
 
     // Ouverture du modal //
-    modalOpeningElements.forEach((element) => {
-        element.addEventListener("click", () => {
-            modal.style.display = "flex";
-            document.body.classList.add("close-modal");
-            overlay.style.display = "block";
+    modalOpeningElements.forEach(function (element) {
+        element.addEventListener("click", function () {
+            modal.style.display = "block";
+            overlay.style.display = "block";  
         });
-    });
+      });
 
     // Fermeture du modal //
-    document.addEventListener("click", (event) => {
-        let clickedOutsideModal = true;
-        
-        modalOpeningElements.forEach((element) => {
-            if (element.contains(event.target)) {
-            clickedOutsideModal = false;
-            }
-        });
-
-        if (clickedOutsideModal && !modal.contains(event.target)) {
-            modal.style.display = "none";
-            document.body.classList.remove("close-modal");
-            overlay.style.display = "none";
-        }
-    });
-
-    closeModalIcons.forEach((icon) => {
-        icon.addEventListener("click", () => {
-            modal.style.display = "none";
-            document.body.classList.remove("close-modal-icon");
-            overlay.style.display = "none";
-        });
-    });
+    
 
 // Appel à L'API pour affichage des photos et créations des balises <img></img> //
 fetch("http://localhost:5678/api/works")
